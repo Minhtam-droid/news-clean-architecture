5bdfd1969b37eb7eac6aeae5bfc32403c8164c00
Hades <Hphamptit@gmail.com>
2020-02-21 01:12:25 +0700

    Migrate Groovy DSL to Kotlin DSL

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,45 @@
+object Config {
+    const val minSdk = 15
+    const val targetSdk = 28
+    const val compileVersion = 29
+    const val buildToolVersion = "29.0.1"
+}
+
+object Versions {
+    val kotlin = "1.3.50"
+    val support_lib = "27.0.2"
+    val retrofit = "2.3.0"
+    val rxjava = "2.1.9"
+}
+
+object Libs {
+    const val kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
+    const val build_gradle = "com.android.tools.build:gradle:3.5.3"
+    const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val core_ktx = "androidx.core:core-ktx:1.2.0"
+    const val junit = "junit:junit:4.12"
+    const val test_junit = "androidx.test.ext:junit:1.1.1"
+    const val test_expresso = "androidx.test.espresso:espresso-core:3.2.0"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:2.1.6"
+    const val rxjava_adapter = "com.squareup.retrofit2:adapter-rxjava2:2.3.0"
+    const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.0.1"
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.3.0"
+    const val logging_interceptor = "com.squareup.okhttp3:logging-interceptor:3.8.0"
+    const val gson_converter = "com.squareup.retrofit2:converter-gson:2.3.0"
+    const val room_runtime = "androidx.room:room-runtime:2.0.0-beta01"
+    const val room_rx = "androidx.room:room-rxjava2:2.0.0-beta01"
+    const val room_kapt_compiler = "androidx.room:room-compiler:2.0.0-beta01"
+    const val mockito = "org.mockito:mockito-core:2.8.47"
+    const val mockito_android = "org.mockito:mockito-android:2.18.3"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0"
+    const val material = "com.google.android.material:material:1.1.0"
+    const val cardview = "androidx.cardview:cardview:1.0.0"
+    const val lifecycle = "androidx.lifecycle:lifecycle-extensions:2.0.0-beta01"
+    const val picasso = "com.squareup.picasso:picasso:2.5.2"
+    const val koin_core = "org.koin:koin-core:2.0.1"
+    const val koin_android = "org.koin:koin-android:2.0.1"
+    const val koin_viewmodel = "org.koin:koin-androidx-viewmodel:2.0.1"
+
+}
